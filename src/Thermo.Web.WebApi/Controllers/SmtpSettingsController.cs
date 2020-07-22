3cc2ef17a0f8a8c804f55c1ebc7cefd053275dd3
jeremywwk <devaksaz@outlook.com>
2020-07-22 23:22:26 +0800

    Added controller to the REST API

--- src/Thermo.Web.WebApi/Controllers/SmtpSettingsController.cs
+++ src/Thermo.Web.WebApi/Controllers/SmtpSettingsController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,16 +8,16 @@ namespace Thermo.Web.WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class SmtpSettingsController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<SmtpSettingsController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public SmtpSettingsController(ILogger<SmtpSettingsController> logger)
         {
             _logger = logger;
         }
